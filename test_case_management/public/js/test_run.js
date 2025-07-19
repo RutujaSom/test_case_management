38dc823b6c06c8b2f5f811e2ad5536cdc09234c9
RutujaSom <rutuja.somvanshi@excellminds.com>
2025-07-19 12:13:22 +0530

    Update test run functionality

--- test_case_management/public/js/test_run.js
+++ test_case_management/public/js/test_run.js
@@ -8,67 +8,6 @@ frappe.ui.form.on('Test Run', {
     }
 });
 
-// function show_test_case_selector(frm) {
-//     const existing_test_cases = (frm.doc.test_cases || []).map(row => row.test_case);
-
-//     new frappe.ui.form.MultiSelectDialog({
-//         doctype: "Test Case",
-//         target: frm,
-//         size: 'large',
-//         setters: {
-//             project: frm.doc.project || ''
-//         },
-//         add_filters_group: 1,
-//         date_field: "creation",
-
-//         // ✅ Custom query that returns name, test_case_id, title
-//         get_query() {
-//             return {
-//                 query: "test_case_management.api.test_case.get_test_cases_query",
-//                 filters: {
-//                     project: frm.doc.project || ''
-//                 }
-//             };
-//         },
-
-//         columns: ["test_case_id", "title"],
-
-//         action(selections) {
-//             alert(selections + " ....")
-//             const to_add = selections.filter(tc => !existing_test_cases.includes(tc));
-
-//             if (to_add.length === 0) {
-//                 frappe.msgprint("No new Test Cases selected.");
-//                 return;
-//             }
-
-//             to_add.forEach(tc => {
-//                 frm.add_child("test_cases", {
-//                     test_case: tc,
-//                     test_case_title: tc.title
-//                 });
-//             });
-
-//             frm.refresh_field("test_cases");
-//             frappe.msgprint(`${to_add.length} Test Case(s) added.`);
-//         }
-//     });
-
-//     // ✅ Auto-select already added test cases
-//     const interval = setInterval(() => {
-//         const checkboxes = $('input[data-name]');
-//         if (checkboxes.length) {
-//             checkboxes.each(function () {
-//                 const rowName = $(this).attr('data-name');
-//                 if (existing_test_cases.includes(rowName)) {
-//                     $(this).prop('checked', true);
-//                 }
-//             });
-//             clearInterval(interval);
-//         }
-//     }, 200);
-// }
-
 function show_test_case_selector(frm) {
     const existing_test_cases = (frm.doc.test_case || []).map(row => row.test_case);
     const checked_values = existing_test_cases.map(name => ({ name }));
@@ -127,7 +66,6 @@ function show_test_case_selector(frm) {
     });
 
     multi_select_dialog.on_page_show = () => {
-        alert('....')
         multi_select_dialog.set_checked_values(checked_values);
         console.log("✅ Pre-selected test cases after data load.");
     };
@@ -135,3 +73,17 @@ function show_test_case_selector(frm) {
 }
 
 
+
+
+frappe.ui.form.on('Test Run', {
+    test_plan: function(frm) {
+        if (frm.doc.test_plan) {
+            frappe.db.get_value('Test Plan', frm.doc.test_plan, 'project')
+                .then(r => {
+                    if (r.message && r.message.project) {
+                        frm.set_value('project', r.message.project);
+                    }
+                });
+        }
+    }
+});
